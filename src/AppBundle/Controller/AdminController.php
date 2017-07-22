3c6ddfa3a6064e19308b92940567765ca4296807
Branko Janjic <branko.janjic@fsd.rs>
2017-07-22 21:25:09 +0200

    Merge branch 'develop' of https://github.com/janjic/PPI into develop
    # Conflicts:
    #	Vagrant/.vagrant/machines/default/virtualbox/action_set_name
    #	app/config/admin/entities/category.yml
    #	composer.json
    #	composer.lock
    #	src/AppBundle/Entity/User.php

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -21,27 +21,6 @@ class AdminController extends EasyAdminController
      */
     public function indexAction(Request $request)
     {
-        $test = new Test();
-        $form = $this->createFormBuilder($test)
-            ->add('images',  ImageCollectionType::class, [
-                'label'            => 'Image Gallery',
-                'context'          => 'entity_images'
-            ]
-            )->getForm();
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($test);
-            $em->flush();
-
-            return $this->redirectToRoute('test_show', array('id' => $test->getId()));
-        }
-
-        return $this->render('@App/test.html.twig', array(
-            'test' => $test,
-            'form' => $form->createView(),
-        ));
         return parent::indexAction($request);
     }
 
