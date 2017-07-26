97cc2858c81253c67ba2bd9b12a1fd8f1cdc69f0
Branko Janjic <branko.janjic@fsd.rs>
2017-07-26 14:11:17 +0200

    Finished dashboard

--- src/AppBundle/Resources/public/js/user.tags.input.js
+++ src/AppBundle/Resources/public/js/user.tags.input.js
@@ -9,6 +9,6 @@ $(function() {
 
     });
 
-    $('#user_roles').prop('multiple', false);
+    //$('#user_roles').prop('multiple', false);
     // $('#user_plainPassword').prop('required', true);
 });
